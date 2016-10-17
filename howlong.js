b052109fbeddad7cc07aae02968331031b788782
Vesa Kilpiäinen <vesa.kilpiainen@soikea.com>
2016-10-17 19:51:31 +0300

    use moment instead of time

--- howlong.js
+++ howlong.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var request = require('request');
-//var time = require('time');
+var moment = require('moment-timezone');
 
 var port = process.env.PORT || 8080;
 var app = express();
@@ -44,22 +44,24 @@ function daydiff(first, second) {
 }
 
 function getTimeRemaining(endtime) {
-  var now = new Date();
-  //var now = new time.Date();
-  //now.setTimezone("Europe/Helsinki");
-  var t = Date.parse(endtime) - Date.parse(now);
-  if(t < 0) return null;
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
+    var tz = "Europe/Helsinki";
+    //var now = new Date();
+    //var now = new time.Date();
+    //now.setTimezone(tz);
+    var now = moment.tz(new Date(), tz).format();
+    var t = Date.parse(endtime) - Date.parse(now);
+    if(t < 0) return null;
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
     'total': t,
     'days': days,
     'hours': hours,
     'minutes': minutes,
     'seconds': seconds
-  };
+    };
 }
 
 
