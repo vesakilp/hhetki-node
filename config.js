f4b2bc1a980c8b5934235195f3583b37cdda070f
Vesa Kilpiäinen <vesakilp@gmail.com>
2017-01-13 05:03:22 +0200

    new config for varpajaiset

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 var config = {};
 config.requestUrl = "https://api.telegram.org/bot162620888:AAEVQ8nB9mFbZmqdJW7FDtLiz1Td83FeYGY/sendMessage";
-config.dday_utc = "2016-10-21 14:00:00";
+config.dday_utc = "2017-01-14 16:00:00";
 config.chatId = "-6480162";
 config.tz = "Europe/Helsinki";
-config.topic = "Stockholm craft cruise";
+config.topic = "Vesan varpajaiset";
 config.messages = [
-    "\n• Siljalla mennään, lähtee Olympiaterminaalista 17.00",
-    "\n• Illalliselle pikkutakki"
+    "\n• 14.1. klo 16:00 Mesikämmenellä",
+    "\n• Tervetulleita ovat kaikki, ilmoittakaa kiitos osallistumisestanne:",
+    "\n• http://doodle.com/poll/8dexw877m3nh3ugh"
 ];
 
 module.exports = config;
